da974dd8f30603740e0a5a80f33acc4504ad4dc4
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-04-19 23:10:30 +0900

    Feat: 채점 결과 데이터에 timestamp 정보 추가

--- assess/app/api/endpoints/games.py
+++ assess/app/api/endpoints/games.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from schemas import common, findroad, rps
 from api.functions import assessment
 import pymongo
+import json
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["test_database"]
@@ -36,29 +37,33 @@ async def get_problem(problem_id: int):
 async def grade_road_game(incoming: findroad.RoadAnswerIncoming):
     problems = incoming.problems
     results = []
+    timestamps = []
     for problem in problems:
         arr = problem.answer
+        timestamp = problem.timestamp
         result = await assessment.find_road(arr)
         # print(result)
         results.append(result['status'])
+        timestamps.append(timestamp)
 
     score = [sum(results), len(results)]  # [맞은 문제수, 푼 문제수]
     
     # MongoDB에 채점 결과 저장
     storing_collection = db["test_road_results"]
-    document = common.GameResult(
+    document = findroad.RoadGameResult(
         id=incoming.game_id, 
         user_id=incoming.user_id, 
         date=incoming.date, 
         game_type=incoming.game_type, 
         results=results, 
+        timestamps=timestamps,
         score=score
         )
 
     # storing_collection.insert_one(document.dict())
 
     content = {
-        "msg": "Game result saved to DB.",
+        "msg": "Road game result saved to DB.",
         "result": document.dict(),
     }
     return JSONResponse(content=content, status_code=200)
@@ -70,6 +75,7 @@ async def grade_rps_game(incoming: rps.RpsAnswer):
 
     # 가위 0, 바위 1, 보 2
     results = []
+    timestamps = []
     for problem in problems:
         answer = problem.answer
         timestamp = problem.timestamp
@@ -80,6 +86,7 @@ async def grade_rps_game(incoming: rps.RpsAnswer):
             results.append(True)
         else:
             results.append(False)
+        timestamps.append(timestamp)
 
     score = [sum(results), len(results)]
 
@@ -91,13 +98,14 @@ async def grade_rps_game(incoming: rps.RpsAnswer):
         date=incoming.date, 
         game_type=incoming.game_type, 
         results=results, 
+        timestamps=timestamps,
         score=score
         )
 
     # storing_collection.insert_one(document.dict())
 
     content = {
-        "msg": "Game result saved to DB.",
+        "msg": "RPS game result saved to DB.",
         "result": document.dict(),
     }
     return JSONResponse(content=content, status_code=200)
