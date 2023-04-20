3050ed2b4b61969eb24431f78d42e9627c677aaf
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-04-20 17:45:32 +0900

    Feat: RPS 시간초과로 미입력시 빈 배열로 받도록 수정

--- assess/app/schemas/rps.py
+++ assess/app/schemas/rps.py
@@ -5,13 +5,13 @@ from schemas.common import ProblemBase, AnswerBase
 
 # 가위바위보 게임 - 문제 입력답의 속성
 class RpsProblem(ProblemBase):
-    answer: List[int]
+    answer: List[str] = []
     timestamp: int
 
     @validator('answer')
     def validate_answer_length(cls, v):
-        if len(v) != 2:
-            raise ValueError("Answer list must have exactly two elements")
+        if len(v) and len(v) != 2:
+            raise ValueError("Answer list must have exactly two elements or no elements")
         return v
 
 
