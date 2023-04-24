714ccb0148fb06c4d3441cd6eb0e1cd1e343771c
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-04-24 16:28:40 +0900

    Feat: Flag 스키마, 모델 추가

---
+++ ability/app/models/model_flag.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+# 모델 클래스 정의
+Base = declarative_base()
+
+
+class Flag(Base):
+    __tablename__ = 'flag'
+    game_id = Column(Integer, primary_key=True)
+    assess = Column(Boolean, default=False)
+    video = Column(Boolean, default=False)
+    is_deleted = Column(Boolean, default=False)
