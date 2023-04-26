220d45adb79bafa527bac518ad686e3997732545
임소정 <sjjj.lim@gmail.com>
2023-04-26 09:30:17 +0900

    Update HelloWorldRecHandler.java

--- recordService/src/main/java/org/kurento/tutorial/helloworld/HelloWorldRecHandler.java
+++ recordService/src/main/java/org/kurento/tutorial/helloworld/HelloWorldRecHandler.java
@@ -394,7 +394,7 @@ public class HelloWorldRecHandler extends TextWebSocketHandler {
         HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(map, headers);
 
         // String serverUrl = "http://localhost:8080/images/upload";
-        String serverUrl = "http://k8a305.p.ssafy.io:8081/images/upload";
+        String serverUrl = "http://172.17.0.1:8081/images/upload";
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.exchange(serverUrl, HttpMethod.POST, requestEntity, String.class);
 
