3b664ddb93a0bd1d0a382d3e4d7f3aea8350797f
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-04-27 16:49:33 +0900

    Update mongodb_cr.py
    경로 수정

--- ai/app/api/functions/mongodb_cr.py
+++ ai/app/api/functions/mongodb_cr.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
-# client = MongoClient("mongodb://mongodb_server:27017/")
-client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb://mongodb_server:27017/")
+# client = MongoClient('localhost', 27017)
 db = client['videos']
 collection = db['videos']
 
@@ -10,6 +10,7 @@ def mongodb_create(data):
     collection.insert_one(data)
 
     return 1
+
 
 def mongodb_read(videoid):
     video = collection.find_one({'_id': videoid})
