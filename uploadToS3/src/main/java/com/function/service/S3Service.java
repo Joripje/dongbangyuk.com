979060ace252e68c54812d9c6df65ccfd22036f0
mmy789 <mmy789@naver.com>
2023-04-27 09:23:03 +0900

    Feat: Content-Type 추가

--- uploadToS3/src/main/java/com/function/service/S3Service.java
+++ uploadToS3/src/main/java/com/function/service/S3Service.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,18 +21,18 @@ public class S3Service {
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucketName;
 
-	private Long sequence = 1L;
-
 	private final Function<MultipartFile, String> generateFileName =
-		file -> "videos/" + file.getOriginalFilename() + sequence++;
+		file -> "videos/" + file.getOriginalFilename();
 
 	public String uploadFileToS3(MultipartFile file) throws IOException {
 		String fileName = generateFileName.apply(file);
-		s3Client.putObject(bucketName, fileName, file.getInputStream(), null);
+		ObjectMetadata objectMetadata = new ObjectMetadata();
+		objectMetadata.setContentType("video/webm");
+		s3Client.putObject(bucketName, fileName, file.getInputStream(), objectMetadata);
 		return fileName;
 	}
 
-	// public Resource readImageFromS3(String filePath) {
+	// public Resource readImageFromS3(String fiQlePath) {
 	// 	try (S3Object s3Object = s3Client.getObject(bucketName, filePath)) {
 	// 		// S3에서 가 져온 객체(파일)를 ByteArrayResource로 변환
 	// 		return new ByteArrayResource(s3Object.getObjectContent().readAllBytes());
