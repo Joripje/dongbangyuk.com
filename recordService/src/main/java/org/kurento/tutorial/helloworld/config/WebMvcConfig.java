e5e7efee1f0f39b5d771f926378ffdeded8622f6
mmy789 <mmy789@naver.com>
2023-05-01 12:25:40 +0900

    Refactor: CORS 임시 허용

---
+++ recordService/src/main/java/org/kurento/tutorial/helloworld/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package org.kurento.tutorial.helloworld.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(false).maxAge(3600);
+	}
+}
