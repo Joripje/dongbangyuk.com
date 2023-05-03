ed2b57433e932bb8fdcff0f47e7ebaa6a0417567
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-05-03 17:42:15 +0900

    Update api.py

--- ability/app/api/api.py
+++ ability/app/api/api.py
@@ -3,4 +3,4 @@ from api.endpoints import flag, ability
 
 api_router = APIRouter()
 api_router.include_router(flag.router, prefix="/flag")
-# api_router.include_router(ability.router, prefix="/ability")
+api_router.include_router(ability.router, prefix="/ability")
