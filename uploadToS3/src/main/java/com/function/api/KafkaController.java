1211e1622b9d7994adfcb2f14dc6c56f8357ec20
mmy789 <mmy789@naver.com>
2023-05-04 10:45:57 +0900

    Fix: 요청 형식 수정

--- uploadToS3/src/main/java/com/function/api/KafkaController.java
+++ uploadToS3/src/main/java/com/function/api/KafkaController.java
@@ -3,10 +3,13 @@ package com.function.api;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequestMapping("/kafka")
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +18,9 @@ public class KafkaController {
 	private final KafkaTemplate<String, String> kafkaTemplate;
 
 	@GetMapping("/publish")
-	public String publish(String message) {
+	public String publish(@RequestParam String message) {
 		this.kafkaTemplate.send("video", message);
+		log.info("message: " + message);
 		return "success";
 	}
 
