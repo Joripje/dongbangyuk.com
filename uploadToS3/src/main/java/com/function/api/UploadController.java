3db69e50a9daa13ceed1ec0a7c344cfd35a4f4cc
mmy789 <mmy789@naver.com>
2023-05-08 08:19:26 +0900

    Refactor: Validation 추가

--- uploadToS3/src/main/java/com/function/api/UploadController.java
+++ uploadToS3/src/main/java/com/function/api/UploadController.java
@@ -1,5 +1,19 @@
 package com.function.api;
 
+import java.io.IOException;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.function.dto.GameSaveRequestDto;
@@ -7,15 +21,10 @@ import com.function.dto.PlaySaveRequestDto;
 import com.function.kafka.GameEventProducer;
 import com.function.service.PlayService;
 import com.function.service.UploadService;
+
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -23,59 +32,64 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class UploadController {
 
-    private final UploadService uploadService;
-    private final PlayService playService;
-    private final GameEventProducer gameEventProducer;
+	private final UploadService uploadService;
+	private final PlayService playService;
+	private final GameEventProducer gameEventProducer;
 
-    @ApiOperation(value = "S3에 영상 업로드")
-    @PostMapping("/upload")
-    public ResponseEntity<?> uploadVideo(@RequestParam("file") MultipartFile file) throws IOException {
-        String filePath = uploadService.uploadVideo(file);
-        gameEventProducer.publish("test", filePath);
-        return ResponseEntity.ok("Video upload successful!");
-    }
+	@ApiOperation(value = "S3에 영상 업로드")
+	@PostMapping("/upload")
+	public ResponseEntity<?> uploadVideo(@RequestParam("file") MultipartFile file) throws IOException {
+		String filePath = uploadService.uploadVideo(file);
+		gameEventProducer.publish("test", filePath);
+		return ResponseEntity.ok("Video upload successful!");
+	}
 
-    @ApiOperation(value = "게임 기록 저장")
-    @PostMapping(value = "/recordPlay")
-    public ResponseEntity<PlaySaveRequestDto> saveGameHistory(@RequestBody PlaySaveRequestDto requestDto) {
+	@ApiOperation(value = "게임 기록 저장")
+	@PostMapping(value = "/recordPlay")
+	public ResponseEntity<PlaySaveRequestDto> saveGameHistory(@Valid @RequestBody PlaySaveRequestDto requestDto) {
+		// 필요한 정보 추출
+		Long userId = requestDto.getUserId();
+		String gameType = requestDto.getGameType();
+		String date = requestDto.getDate();
 
-        ObjectMapper objectMapper = new ObjectMapper();
+		// GameHistory 엔티티 생성 후 필요한 정보 저장
+		GameSaveRequestDto gameHistory = createGameHistory(userId, gameType, date);
 
-        // 필요한 정보 추출
-        Long userId = requestDto.getUserId();
-        String gameType = requestDto.getGameType();
-        String date = requestDto.getDate();
+		// GameHistory 엔티티 저장
+		Long newGameId = playService.save(gameHistory);
+		requestDto.setGameId(newGameId);
 
-        // GameHistory 엔티티 생성 후 필요한 정보 저장
-        GameSaveRequestDto gameHistory = GameSaveRequestDto.builder()
-                .userId(userId)
-                .gameType(gameType)
-                .date(date)
-                .build();
+		String dtoString = convertDtoToJsonString(requestDto);
+		log.info("gameEventProducer 호출");
+		gameEventProducer.publish("test", dtoString);
 
-        // GameHistory 엔티티 저장
-        Long newGameId = playService.save(gameHistory);
-        requestDto.setGameId(newGameId);
+		return ResponseEntity.ok(requestDto);
+	}
 
-        log.info("gameEventProducer 호출");
-        String dtoString;
-        try {
-            dtoString = objectMapper.writeValueAsString(requestDto);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        gameEventProducer.publish("test", dtoString);
-        return ResponseEntity.ok(requestDto);
-    }
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+	}
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
+	@ExceptionHandler(IOException.class)
+	public ResponseEntity<String> handleIOException(IOException e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Image upload failed: " + e.getMessage());
+	}
 
-    @ExceptionHandler(IOException.class)
-    public ResponseEntity<String> handleIOException(IOException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Image upload failed: " + e.getMessage());
-    }
+	private GameSaveRequestDto createGameHistory(Long userId, String gameType, String date) {
+		return GameSaveRequestDto.builder()
+			.userId(userId)
+			.gameType(gameType)
+			.date(date)
+			.build();
+	}
+
+	private String convertDtoToJsonString(Object dto) {
+		try {
+			return new ObjectMapper().writeValueAsString(dto);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException("Failed to convert Dto to JSON string", e);
+		}
+	}
 
 }
--- uploadToS3/src/main/java/com/function/dto/PlaySaveRequestDto.java
+++ uploadToS3/src/main/java/com/function/dto/PlaySaveRequestDto.java
@@ -1,20 +1,29 @@
 package com.function.dto;
 
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.databind.JsonNode;
+
 import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.List;
 
 @ApiModel(description = "게임 결과 데이터")
 @Getter
 public class PlaySaveRequestDto {
 
+	@NotNull(message = "UserID must not be null")
 	private final Long userId;
+
+	@NotNull(message = "Date must not be null")
 	private final String date;
+
+	@NotNull(message = "GameType must not be null")
 	private final String gameType;
-    private final List<JsonNode> problems;
+
+	private final List<JsonNode> problems;
 	private Long gameId = 0L;
 
 	@Builder
@@ -22,7 +31,7 @@ public class PlaySaveRequestDto {
 		this.userId = userId;
 		this.date = date;
 		this.gameType = gameType;
-        this.problems = problems;
+		this.problems = problems;
 	}
 
 	public void setGameId(Long gameId) {
