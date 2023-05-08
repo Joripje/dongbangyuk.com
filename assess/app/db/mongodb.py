9c360ad2f6bf4c7e956cf55a96fad3117ec2bf20
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-05-08 13:33:56 +0900

    Update: mongodb 권한 추가

--- assess/app/db/mongodb.py
+++ assess/app/db/mongodb.py
@@ -1,6 +1,14 @@
 import pymongo
+from dotenv import load_dotenv
+import os
 
-client = pymongo.MongoClient("mongodb://mongodb_server:27017/")
+load_dotenv()
+
+mongo_username = os.environ.get('MONGO_USERNAME')
+mongo_password = os.environ.get('MONGO_PASSWORD')
+
+# client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@k8a305.p.ssafy.io:27017/")
+client = pymongo.MongoClient(f"mongodb://{mongo_username}:{mongo_password}@mongodb_server:27017/")
 # client = pymongo.MongoClient("mongodb://localhost:27017/")
 problem_db = client["game_problem"]
 result_db = client["game_result"]
