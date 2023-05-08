d34aecbd4996172019711d86a2c95ebc6c6432a0
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-05-08 17:48:40 +0900

    Feat: MongoDB 보안 설정

--- assess/app/db/mongodb.py
+++ assess/app/db/mongodb.py
@@ -1,6 +1,16 @@
-import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-client = pymongo.MongoClient("mongodb://mongodb_server:27017/")
-# client = pymongo.MongoClient("mongodb://localhost:27017/")
+load_dotenv()
+
+mongo_username = os.environ.get('MONGO_USERNAME')
+mongo_password = os.environ.get('MONGO_PASSWORD')
+
+# 원격 서버
+client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@mongodb_server:27017/")
+# 로컬 테스트
+# client = MongoClient("mongodb://localhost:27017/")
+
 problem_db = client["game_problem"]
 result_db = client["game_result"]
