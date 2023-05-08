442069684bf758d751fcac4b8becf417419ea8cd
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-05-08 12:33:20 +0900

    Feat: get 요청 분리

--- front/src/api/apiget.tsx
+++ front/src/api/apiget.tsx
@@ -4,7 +4,8 @@ const headers = {
 
 // const baseUrl = "http://localhost:8000/";
 // const baseUrl = "http://70.12.246.183:8000";
-const baseUrl = "https://k8a305.p.ssafy.io";
+// const baseUrl = "https://k8a305.p.ssafy.io";
+const baseUrl = "http://k8a305.p.ssafy.io:8020";
 
 interface RequestProps {
   method: string;
@@ -14,34 +15,30 @@ interface RequestProps {
   };
 }
 
-const request = async (props: RequestProps) => {
-  const { method, data, url } = props;
-  const options = {
-    method,
-    headers,
-    body: JSON.stringify(data),
-  };
-
-  try {
-    const response = await fetch(baseUrl + url, options);
-    if (!response.ok) {
-      throw new Error(`HTTP error!: ${response}`);
-    }
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const requestGet = async (props: RequestProps) => {
+export default async function request(props: RequestProps) {
   const { method, data, url } = props;
   console.log(data);
   const options = {
     method,
     headers,
+    [method === "GET" ? "param" : "body"]: JSON.stringify(data),
   };
   let requestUrl = baseUrl + url;
+
+  // if (data && Object.keys(data).length > 0) {
+  //   requestUrl += "?";
+  //   let isFirstParam = true;
+  //   for (const key in data) {
+  //     if (data.hasOwnProperty(key)) {
+  //       if (!isFirstParam) {
+  //         requestUrl += "&";
+  //       }
+  //       requestUrl += key + "=";
+  //       requestUrl += data[key];
+  //       isFirstParam = false;
+  //     }
+  //   }
+  // }
 
   if (data && Object.keys(data).length > 0) {
     const searchParams = new URLSearchParams();
@@ -64,6 +61,4 @@ const requestGet = async (props: RequestProps) => {
   } catch (error) {
     console.error(error);
   }
-};
-
-export { request, requestGet };
+}
