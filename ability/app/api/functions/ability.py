227c83973e33cbf18a75d3d31426c8b1abb1e7d3
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-05-08 13:22:25 +0900

    Update ability.py

--- ability/app/api/functions/ability.py
+++ ability/app/api/functions/ability.py
@@ -10,8 +10,8 @@ mongo_username = os.environ.get('MONGO_USERNAME')
 mongo_password = os.environ.get('MONGO_PASSWORD')
 
 # 몽고디비에 저장하는 코드
-client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@k8a305.p.ssafy.io:27017/")
-# client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@mongodb_server:27017/")
+# client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@k8a305.p.ssafy.io:27017/")
+client = MongoClient(f"mongodb://{mongo_username}:{mongo_password}@mongodb_server:27017/")
 # client = MongoClient('localhost', 27017)
 
 db = client['ability']
