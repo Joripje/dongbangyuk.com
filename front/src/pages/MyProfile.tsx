344a64aa66ed871388110642081e51bcdb9c9db2
seotai78@naver.com <seotai78@naver.com>
2023-05-11 16:09:02 +0900

    feat: 채팅 기능 css완

--- front/src/pages/MyProfile.tsx
+++ front/src/pages/MyProfile.tsx
@@ -1,15 +1,54 @@
 import React from "react";
-
+import styled from "styled-components";
 import { Navbar } from "components/common";
 import { UserInfo } from "components/profile";
+import { AbilityChart } from "components/statistics";
+
+import { auth } from "service";
 
 function MyProfile() {
   return (
     <div>
       <Navbar />
       <UserInfo />
+      <AbilityBox>
+        <AbilityChart />
+      </AbilityBox>
+      <InfoBox>
+        <h1>user info</h1>
+        {/* <Info>이메일</Info> */}
+      </InfoBox>
     </div>
   );
 }
+const AbilityBox = styled.div({
+  backgroundColor: "#D9F7F3",
+  width: "37vw",
+  height: "40%",
+  display: "flex",
+  marginTop: "4rem",
+  marginLeft: "13rem",
+  borderRadius: "1rem",
+  justifyContent: "center",
+  position: "fixed",
+});
+const InfoBox = styled.div({
+  backgroundColor: "#D9F7F3",
+  width: "37vw",
+  height: "40%",
+  display: "flex",
+  marginTop: "4rem",
+  marginLeft: "67rem",
+  borderRadius: "1rem",
+  justifyContent: "center",
+  position: "fixed",
+});
 
+const Info = styled.div({
+  borderRadius: "1rem",
+  backgroundColor: "#FFB4B4",
+  color: "black",
+  width: "20rem",
+  height: "5rem",
+});
 export default MyProfile;
