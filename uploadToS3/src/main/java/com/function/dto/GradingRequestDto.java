7af2a730a2c6abcadcf392213d9a21c7e6b2fc04
mmy789 <mmy789@naver.com>
2023-05-11 14:41:30 +0900

    Fix: error 해결

---
+++ uploadToS3/src/main/java/com/function/dto/GradingRequestDto.java
@@ -0,0 +1,30 @@
+package com.function.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Builder;
+import lombok.Getter;
+
+@ApiModel(description = "채점 요청 데이터")
+@Getter
+public class GradingRequestDto {
+
+	private final Long gameId;
+	private final String date;
+	private final Long userId;
+	private final String gameType;
+	private final List<JsonNode> problems;
+
+	@Builder
+	public GradingRequestDto(Long gameId, PlaySaveRequestDto requestDto) {
+		this.gameId = gameId;
+		this.date = requestDto.getDate();
+		this.userId = requestDto.getUserId();
+		this.gameType = requestDto.getGameType();
+		this.problems = requestDto.getProblems();
+	}
+
+}
