2d1e95838b36f548d03e030223e2e0e2b1f8db2e
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-05-12 16:55:01 +0900

    Feat: 길일 추천 API

---
+++ assess/app/api/endpoints/unse.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from bs4 import BeautifulSoup
+import requests, re
+
+router = APIRouter()
+
+url = 'https://shinhanlife.sinbiun.com/unse/good_luck.php'
+
+
+@router.post("/unse/luckyday", description='생년월일과 타겟날짜를 8자리 숫자로 입력, 성별 입력(M/F)')
+async def send_message_to_topic(birth: str, target: str, gender: str):
+    birth_year, birth_month, birth_day = birth[:4], birth[4:6], birth[6:8]
+    target_year, target_month, target_day = target[:4], target[4:6], target[6:8]
+    params = {
+        'unse_code':'A103',
+        'name':'고객',
+        'specific_year': target_year,
+        'specific_month': target_month,
+        'specific_day': target_day,
+        'user_gender': gender,
+        'user_birth_year': birth_year,
+        'gender': gender,
+        'sl_cal':'S',
+        'birth_year': birth_year,
+        'birth_month': birth_month,
+        'birth_day': birth_day,
+        'birth_hour':'02',
+        'sp_num': f'{target_year}-{target_month}'
+    }
+    
+    response = requests.post(url, params=params)
+    if response.status_code == 200:
+        response_content = response.content
+    else:
+        response.raise_for_status()
+    
+    soup = BeautifulSoup(response_content, 'html.parser')
+
+    lucky_day_div = soup.find('div', {'data-acc-view': 'acc_con14'})
+    content_div = lucky_day_div.find('div', class_='content')
+    content_text = content_div.get_text(strip=True)
+
+    sentences = list(content_text.split('일'))
+    dates = []
+    for s in sentences:
+        numbers = re.findall('\d+', s)
+        if numbers:
+            numbers = [int(n) for n in numbers]
+            dates.append(numbers)
+
+    lucky_dates = { 'month': int(target_month) }
+    lucky_dates['lucky_dates'] = [
+        {'type': 'job', 'description': '취업이나 면접 등에 좋은 날', 'dates': dates[1]},
+        {'type': 'money', 'description': '재물의 흐름이 좋은 날', 'dates': dates[2]},
+        {'type': 'heart', 'description': '이성 만남에 좋은 날', 'dates': dates[4]},
+        {'type': 'overall', 'description': '이번 달 운의 종합적인 흐름', 'dates': dates[5]},
+        {'type': 'helper', 'description': '운의 흐름과 관계없이 귀인이 들어 도움을 주는 날', 'dates': dates[6]},
+        {'type': 'avoid_most', 'description': '이번 달 중 반드시 피해야 하는 날', 'dates': dates[7]},
+        {'type': 'avoid_second', 'description': '그 다음으로 피해야하는 날', 'dates': dates[8]},
+    ]
+
+    return JSONResponse(content=lucky_dates, status_code=200)
