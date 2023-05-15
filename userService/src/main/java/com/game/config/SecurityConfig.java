2e0e542aaa5b034181e94a7d1b61917f20d90adf
mmy789 <mmy789@naver.com>
2023-05-15 16:19:53 +0900

    Refactor: SecurityConfig 변경

--- userService/src/main/java/com/game/config/SecurityConfig.java
+++ userService/src/main/java/com/game/config/SecurityConfig.java
@@ -29,29 +29,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	/**
 	 * HttpRequest를 받는 부분에 filter 적용 (addFilterBefore)
 	 */
-	// @Override
-	// public void configure(HttpSecurity http) throws Exception {
-	// 	http.cors().and()
-	// 		.authorizeRequests()
-	// 			.antMatchers(HttpMethod.POST, "/users/register", "/users/register/").permitAll()
-	// 			.antMatchers(HttpMethod.POST, "/users/profile-update").authenticated()
-	// 			.anyRequest().authenticated()
-	// 			.and()
-	// 		.addFilterBefore(new FirebaseTokenFilter(userService, firebaseAuth),
-	// 			UsernamePasswordAuthenticationFilter.class)
-	// 		.exceptionHandling()
-	// 		.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-	// }
-
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest().authenticated().and()
+		http.cors().and()
+			.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/users/register", "/users/register/").permitAll()
+				.antMatchers(HttpMethod.POST, "/users/profile-update").authenticated()
+				.anyRequest().authenticated()
+				.and()
 			.addFilterBefore(new FirebaseTokenFilter(userService, firebaseAuth),
 				UsernamePasswordAuthenticationFilter.class)
 			.exceptionHandling()
 			.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 	}
+
+	// @Override
+	// public void configure(HttpSecurity http) throws Exception {
+	// 	http.authorizeRequests()
+	// 		.anyRequest().authenticated().and()
+	// 		.addFilterBefore(new FirebaseTokenFilter(userService, firebaseAuth),
+	// 			UsernamePasswordAuthenticationFilter.class)
+	// 		.exceptionHandling()
+	// 		.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+	// }
 
 	/**
 	 * WebSecurity를 받는 부분에서 Filter를 적용하지 않을 요청
