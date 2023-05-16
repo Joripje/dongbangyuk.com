0afe2c5fb7677b11c81cdac3da15385eaf25bc98
mmy789 <mmy789@naver.com>
2023-05-16 11:40:56 +0900

    Docs: 로그 추가

--- uploadToS3/src/main/java/com/function/api/PlayController.java
+++ uploadToS3/src/main/java/com/function/api/PlayController.java
@@ -14,7 +14,9 @@ import com.function.service.PlayService;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/plays")
 @RequiredArgsConstructor
@@ -25,18 +27,24 @@ public class PlayController {
 	@ApiOperation(value = "{id}에 해당하는 게임 기록 가져오기")
 	@GetMapping("/{id}")
 	public ResponseEntity<PlayListResponseDto> getOnePlayById(@PathVariable("id") Long id) {
+		log.info("======== getOnePlayById 호출 ========");
+		System.out.println("id : " + id);
 		return ResponseEntity.ok(playService.findById(id));
 	}
 
 	@ApiOperation(value = "{gameId}에 해당하는 유저 아이디 가져오기")
 	@GetMapping("/userInfo")
 	public ResponseEntity<Long> getUserIdByGameId(@RequestParam Long gameId) {
+		log.info("======== getUserIdByGameId 호출 ========");
+		System.out.println("gameId : " + gameId);
 		return ResponseEntity.ok(playService.findById(gameId).getUserId());
 	}
 	
 	@ApiOperation(value = "아이디가 {id}인 유저의 모든 게임 기록 가져오기")
 	@GetMapping("/users/{id}")
 	public ResponseEntity<List<PlayListResponseDto>> getPlayByUserId(@PathVariable("id") Long userId) {
+		log.info("======== getPlayByUserId 호출 ========");
+		System.out.println("userId : " + userId);
 		return ResponseEntity.ok(playService.findByUserId(userId));
 	}
 
