25e99e6f899d2e3fac04507f21e166926b0e98a4
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-05-17 16:48:21 +0900

    Feat: 알림 버튼 클릭 시 뱃지 제거

--- front/src/components/notification/NotificationButton.tsx
+++ front/src/components/notification/NotificationButton.tsx
@@ -22,6 +22,7 @@ const NotificationButton = () => {
           userid: userId,
         });
         setNotificationList(response);
+        setNewNotificationCount(0);
       } catch (err) {
         console.error(err);
       }
