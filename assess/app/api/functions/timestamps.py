27320ba7dcad196a0ca4e38a79ed8cd085ff59ff
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-05-17 13:03:57 +0900

    Feat: 정규화된 timestamp를 int로 반환하도록 수정

--- assess/app/api/functions/timestamps.py
+++ assess/app/api/functions/timestamps.py
@@ -3,8 +3,11 @@ from datetime import datetime
 
 def time_normalization(timestamps: list):
     ref_time = timestamps[0][0]
-    normalized_timestamps = []
-    for timestamp in timestamps:
-        normalized = [(timestamp[0] - ref_time).total_seconds(), (timestamp[1] - ref_time).total_seconds()]
-        normalized_timestamps.append(normalized)
-    return normalized_timestamps
+
+    for i, timestamp in enumerate(timestamps):
+        start, end = timestamp
+        start = round((start - ref_time).total_seconds())
+        end = round((end - ref_time).total_seconds())
+        timestamps[i] = [start, end]
+    
+    return timestamps
