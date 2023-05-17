9e41b50af6230d82dcd643cfcf8a5f67196a858f
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2023-05-17 17:51:45 +0900

    Feat: 데이터 없을 때 상황 추가

--- front/src/components/statistics/TotalAbilityChart.tsx
+++ front/src/components/statistics/TotalAbilityChart.tsx
@@ -1,5 +1,7 @@
 import { getTotalAbilityData } from "api/statistics";
 import { useState, useEffect } from "react";
+import styled from "styled-components";
+
 import {
   Radar,
   RadarChart,
@@ -19,6 +21,7 @@ const TotalAbilityChart = (props: TotalAbilityChartProps) => {
   const [catScore, setCatScore] = useState<number>(0);
   const [resilienceAvg, setResilienceAvg] = useState<number>(0);
   const [enduranceAvg, setEnduranceAvg] = useState<number>(0);
+  const [isNoData, setIsNoData] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,8 +36,9 @@ const TotalAbilityChart = (props: TotalAbilityChartProps) => {
         setCatScore(response.catScore);
         setResilienceAvg(response.resilienceAvg);
         setEnduranceAvg(response.enduranceAvg);
+        setIsNoData(false);
       } catch (err) {
-        console.error(err);
+        setIsNoData(true);
       }
     };
 
@@ -81,8 +85,36 @@ const TotalAbilityChart = (props: TotalAbilityChartProps) => {
           fillOpacity={0.6}
         />
       </RadarChart>
+      {isNoData === true && (
+        <BoardBox>
+          4가지 게임 응시 후<br />
+          분석이 완료되어야 합니다.
+        </BoardBox>
+      )}
     </>
   );
 };
+const BoardBox = styled.div({
+  position: "absolute",
+  top: "45%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  margin: "1rem auto",
+  display: "flex",
+  flexDirection: "row",
+  padding: "1rem 0",
 
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+
+  fontSize: "1.1rem",
+  fontWeight: "bold",
+
+  width: "300px",
+  height: "3rem",
+  background: "white",
+  borderRadius: 10,
+  boxShadow: "5px 5px 5px rgba(0, 0, 0, 0.2)",
+});
 export default TotalAbilityChart;
