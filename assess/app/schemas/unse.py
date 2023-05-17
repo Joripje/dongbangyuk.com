d9b80c776911cf8ff55d71268e16602231e34735
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2023-05-17 11:01:38 +0900

    Feat: 운세 API validation 추가

--- assess/app/schemas/unse.py
+++ assess/app/schemas/unse.py
@@ -1,12 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from datetime import datetime
+from schemas.common import to_camel_case
 
 
-def to_camel_case(string: str) -> str:
-    string_split = string.split("_")
-    return string_split[0] + "".join(word.capitalize() for word in string_split[1:])
-
-
-# 게임 문제의 공통 속성
 class UnseModel(BaseModel):
     birth: str
     target: str
@@ -14,3 +10,17 @@ class UnseModel(BaseModel):
 
     class Config:
         alias_generator = to_camel_case
+
+    @validator('birth', 'target')
+    def validate_date(cls, value):
+        try:
+            datetime.strptime(value, '%Y%m%d')
+        except ValueError:
+            raise ValueError("Invalid date format. Expected format: YYYYMMDD")
+        return value
+
+    @validator('gender')
+    def validate_gender(cls, value):
+        if value not in ['M', 'F']:
+            raise ValueError("Invalid gender format. Expected format: 'M' or 'F'")
+        return value
