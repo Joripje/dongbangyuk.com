9a32b53c57a57ee5ace9bf2efd8ca26fbad3651e
mmy789 <mmy789@naver.com>
2023-05-18 11:17:55 +0900

    Refactor: cors 설정 변경

--- userService/src/main/java/com/game/config/CorsConfig.java
+++ userService/src/main/java/com/game/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("https://k8a305.p.ssafy.io")
+			.allowedOrigins("*")
 			.allowedMethods("*")
 			.allowedHeaders("*")
 			.exposedHeaders("*")
