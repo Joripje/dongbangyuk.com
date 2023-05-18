5aaa649ee01497e91a00eeaec99946186662fd44
yadon <mucho7@gmail.com>
2023-05-18 16:11:08 +0900

    Feat: 통계리스트 페이지를 위한 데이터 소프트 코딩

--- front/src/pages/StatisticsListPage.tsx
+++ front/src/pages/StatisticsListPage.tsx
@@ -23,7 +23,8 @@ const StatisticsListPage = () => {
     total: 0,
   });
   const location = useLocation();
-  const parsed = queryString.parse(location.search);
+  // const parsed = queryString.parse(location.search);
+  const parsed = { userid: 20 };
   // console.log(parsed);
 
   const TypeChangeHandler = (gameType: string) => {
