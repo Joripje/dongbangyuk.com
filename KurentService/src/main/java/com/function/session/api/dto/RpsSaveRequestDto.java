1b2ec48f8d86f18463263e9153a6dfb4c34ff2a9
mmy789 <mmy789@naver.com>
2023-05-18 10:38:52 +0900

    Refactor: GameResults Dto 분리

---
+++ KurentService/src/main/java/com/function/session/api/dto/RpsSaveRequestDto.java
@@ -0,0 +1,41 @@
+package com.function.session.api.dto;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+
+@ApiModel(description = "가위바위보 결과 데이터")
+@Getter
+public class RpsSaveRequestDto {
+
+	@NotNull(message = "gameId must not be null")
+	private final int gameId;
+
+	@NotNull(message = "UserID must not be null")
+	private final int userId;
+
+	@NotNull(message = "GameType must not be null")
+	private final String gameType;
+
+	private final List<JsonNode> rounds;
+
+	@JsonCreator
+	public RpsSaveRequestDto(
+		@JsonProperty("gameId") String gameId,
+		@JsonProperty("userId") int userId,
+		@JsonProperty("gameType") String gameType,
+		@JsonProperty("rounds") List<JsonNode> rounds) {
+		this.gameId = Integer.parseInt(gameId);
+		this.userId = userId;
+		this.gameType = gameType;
+		this.rounds = rounds;
+	}
+
+}
