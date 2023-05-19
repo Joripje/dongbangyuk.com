4de80d0eb464e4a2bcdc3fec7eab9356d92701f2
yadon <mucho7@gmail.com>
2023-05-19 10:33:08 +0900

    Feat: 운세 리팩토링 종료

--- front/src/components/unse/UnseControl.tsx
+++ front/src/components/unse/UnseControl.tsx
@@ -12,6 +12,7 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 type Luckyday = {
   description: string;
@@ -25,34 +26,25 @@ type UnseControlProps = {
 
 function UnseControl(props: UnseControlProps) {
   const { setLuckyday, setTodayluck } = props;
-  const [gender, setGender] = useState<string>("");
-  const [selectedDate, setSelectedDate] = useState<string | null>("male");
-  const [today] = useState(new Date().toISOString().slice(0, 10));
+  const [gender, setGender] = useState<string>("M");
+  const [selectedDate, setSelectedDate] = useState<any>(null);
 
   const handleGender = (event: ChangeEvent) => {
     event.preventDefault();
     setGender(event.currentTarget.id);
-    console.log(selectedDate);
   };
 
   const handleLuck = (e: any) => {
     e.preventDefault();
-    // console.log(e.target.elements);
-    const MF = gender;
-    const birth = selectedDate;
-    const target = today.replace(/-/g, "");
+    const tempData = {
+      birth: selectedDate,
+      target: new Date().toISOString().slice(0, 10).replace(/-/g, ""),
+      gender: gender,
+    };
     const fetchData = async () => {
       try {
-        const response = await postLuckyday({
-          birth: birth,
-          target: target,
-          gender: MF,
-        });
-        const response2 = await postToday({
-          birth: birth,
-          target: target,
-          gender: MF,
-        });
+        const response = await postLuckyday(tempData);
+        const response2 = await postToday(tempData);
         setLuckyday(response.lucky_dates);
         setTodayluck(response2);
       } catch (e) {
@@ -62,15 +54,21 @@ function UnseControl(props: UnseControlProps) {
     fetchData();
   };
 
+  const handleChange = (value: Date | null) => {
+    console.log(value !== null ? dayjs(value).format("YYYYMMDD") : "");
+    setSelectedDate(value !== null ? dayjs(value).format("YYYYMMDD") : "");
+  };
+
   return (
     <ControlBox>
       <ColFlex>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DemoContainer components={["DatePicker"]}>
             <DatePicker
+              value={selectedDate}
               label={"생년월일"}
               format='YYYY/MM/DD'
-              onChange={(value: string | null) => setSelectedDate(value)}
+              onChange={handleChange}
             />
           </DemoContainer>
         </LocalizationProvider>
@@ -81,14 +79,14 @@ function UnseControl(props: UnseControlProps) {
           >
             <FormControlLabel
               id='male'
-              control={<Radio id='male' />}
+              control={<Radio id='/m' />}
               label='남자'
-              value={"male"}
+              value={"/m"}
             />
             <FormControlLabel
-              control={<Radio id='female' />}
+              control={<Radio id='F' />}
               label='여자'
-              value={"female"}
+              value={"F"}
             />
           </RadioGroup>
         </FormControl>
